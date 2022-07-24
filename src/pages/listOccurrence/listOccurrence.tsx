56b9f7569d20dae22bf7a5b435f1a76521e256c4
Guillaume Zeldine <g.zeldine@hotmail.fr>
2022-07-23 23:00:17 -0300

    Added button to add new Occurrence

--- src/pages/listOccurrence/listOccurrence.tsx
+++ src/pages/listOccurrence/listOccurrence.tsx
@@ -20,12 +20,20 @@ function ListOccurrence() {
       }
     }, [])
 
+  const addNewOccurrence = () => {
+    const newIDate = { begin: new Date(), end: new Date()} as any as IDate
+    setTimeEntries(timeEntries.concat(newIDate))
+  }
+
   return (
     <div className="listOccurrence bg-cyan-100">
       <div className="grid place-content-center h-screen w-screen">
         <div className="container shadow-md p-10">
-
-          <label className="font-bold"> List of occurrences: </label>
+          <div className="flex justify-between">
+            <label className="font-bold"> List of occurrences: </label>
+            <button onClick={addNewOccurrence} className="p-2 rounded text-zinc-50 bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300">
+              New Occurrence</button>
+          </div>
 
           <div className="occurrenceContext my-5 overflow-y-scroll">
               {timeEntries && timeEntries.map(timeEntry =>
