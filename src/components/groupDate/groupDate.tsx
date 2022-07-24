f713a0735e0bea9ec55acd03243e1c2c4fce2b53
Guillaume Zeldine <g.zeldine@hotmail.fr>
2022-07-23 22:12:26 -0300

    GroupDate border rounded, removed unused div

--- src/components/groupDate/groupDate.tsx
+++ src/components/groupDate/groupDate.tsx
@@ -6,49 +6,47 @@ function GroupDate( {label, begin, end}: IDate ) {
   const beginDate = new Date(begin).toLocaleString();
   const endDate = new Date(end).toLocaleString();
 
-    return (
-      <>
-        <div className="groupDate">
+  return (
+    <div className="groupDate">
 
-          <div className="currentDate p-10">
+      <div className="currentDate p-10">
 
-            <fieldset className='border-2 border-sky-600 hover:border-sky-900 p-5'>
+        <fieldset className='border-2 border-sky-600 hover:border-sky-900 rounded p-5'>
 
-              <legend className="mb-5 font-bold"> Data: {label} </legend>
+          <legend className="mb-5 font-bold"> Data: {label} </legend>
 
-              <div className="grid gap-2 lg:grid-cols-2 sm:grid-cols-1">
+          <div className="grid gap-2 lg:grid-cols-2 sm:grid-cols-1">
 
-                <div className="slot-1">
+            <div className="slot-1">
 
-                  <label> Initial:
-                    <input type="text"
-                      className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                      size={50}
-                      name="txtDateInitial"
-                      id="txtDateInitial"
-                      value={beginDate}
-                    />
-                  </label>
-                </div>
+              <label> Initial:
+                <input type="text"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                  size={50}
+                  name="txtDateInitial"
+                  id="txtDateInitial"
+                  defaultValue={beginDate}
+                />
+              </label>
+            </div>
 
-                <div className="slot-2">
+            <div className="slot-2">
 
-                  <label> Final:
-                    <input type="text"
-                      className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                      size={50}
-                      name="txtDateFinal"
-                      id="txtDateFinal"
-                      value={endDate}
-                    />
-                  </label>
-                </div>
-              </div>
-            </fieldset>
+              <label> Final:
+                <input type="text"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                  size={50}
+                  name="txtDateFinal"
+                  id="txtDateFinal"
+                  defaultValue={endDate}
+                />
+              </label>
+            </div>
           </div>
-        </div>
-      </>
-    )
-  }
+        </fieldset>
+      </div>
+    </div>
+  )
+}
 
 export {GroupDate}
--- src/pages/listOccurrence/listOccurrence.tsx
+++ src/pages/listOccurrence/listOccurrence.tsx
@@ -9,8 +9,9 @@ export type IDate = {
   end: string
 }
 
-function ListOccurrence(props: any) {
+function ListOccurrence() {
   const [timeEntries, setTimeEntries] = useState<IDate[]>([])
+  let index = 0
   const {state}: any = useLocation()
 
   useEffect(() => {
@@ -19,30 +20,26 @@ function ListOccurrence(props: any) {
       }
     }, [])
 
-    return (
-      <div className="listOccurrence bg-cyan-100">
-        <div className="grid place-content-center h-screen w-screen">
-          <div className="container shadow-md p-10">
+  return (
+    <div className="listOccurrence bg-cyan-100">
+      <div className="grid place-content-center h-screen w-screen">
+        <div className="container shadow-md p-10">
 
-            <label className="font-bold"> List of occurrences: </label>
+          <label className="font-bold"> List of occurrences: </label>
 
-            <div className="occurrenceContext my-5 overflow-y-scroll">
-              <>
-                {timeEntries && timeEntries.map(timeEntry => {
-                  return (
-                    <div> <GroupDate label={timeEntry.label} begin={timeEntry.begin} end={timeEntry.end} key={timeEntry.label}/> </div>
-                  )
-                })}
-              </>
-            </div>
-
-            <button className="p-2 w-full rounded text-zinc-50 bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300">
-                Continue
-            </button>
+          <div className="occurrenceContext my-5 overflow-y-scroll">
+              {timeEntries && timeEntries.map(timeEntry =>
+                <GroupDate label={timeEntry.label} begin={timeEntry.begin} end={timeEntry.end} key={index++}/>
+              )}
           </div>
+
+          <button className="p-2 w-full rounded text-zinc-50 bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300">
+              Continue
+          </button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 export default ListOccurrence
